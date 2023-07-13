1607c7b3a12b7ae11dda42bcc849bee226511b6d
Vladimir <valjanikov-vladimir@yandex.ru>
2023-07-13 22:51:12 +0300

    Добавьте файлы проекта.

---
+++ AuthenticationService/Controllers/UserController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace AuthenticationService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        private ILogger _logger;
+        public UserController(
+            ILogger logger)
+        {
+            _logger = logger;
+
+            logger.WriteEvent("Сообщение о событии в программе");
+            logger.WriteError("Сообщение об ошибки в программе");
+
+        }
+
+        [HttpGet]
+        public User GetUser()
+        {
+            return new User()
+            {
+                Id = Guid.NewGuid(),
+                FirstName = "Иван",
+                LastName = "Иванов",
+                Email = "ivan@gmail.com",
+                Password = "11111122222qq",
+                Login = "ivanov"
+            };
+        }
+
+        [HttpGet]
+        [Route("viewmodel")]
+        public UserViewModel GetUserViewModel()
+        {
+            User user = new User()
+            {
+                Id = Guid.NewGuid(),
+                FirstName = "Иван",
+                LastName = "Иванов",
+                Email = "ivan@gmail.com",
+                Password = "11111122222qq",
+                Login = "ivanov"
+            };
+
+            UserViewModel userViewModel = new UserViewModel(user);
+
+            return userViewModel;
+        }
+    }
+}
